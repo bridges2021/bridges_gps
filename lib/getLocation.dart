47763b236f83c76d070d0b27961ed1a9dc336a00
fanchungchit <66772739+fanchungchit@users.noreply.github.com>
2021-08-18 12:00:42 +0800

    add another package support

---
+++ lib/getLocation.dart
@@ -0,0 +1,28 @@
+import 'package:location/location.dart';
+
+Future<LocationData> getLocation() async {
+  Location location = new Location();
+
+  bool _serviceEnabled;
+  PermissionStatus _permissionGranted;
+  LocationData _locationData;
+
+  _serviceEnabled = await location.serviceEnabled();
+  if (!_serviceEnabled) {
+    _serviceEnabled = await location.requestService();
+    if (!_serviceEnabled) {
+      throw 'Service not available';
+    }
+  }
+
+  _permissionGranted = await location.hasPermission();
+  if (_permissionGranted == PermissionStatus.denied) {
+    _permissionGranted = await location.requestPermission();
+    if (_permissionGranted != PermissionStatus.granted) {
+      throw 'Permission denied';
+    }
+  }
+
+  _locationData = await location.getLocation();
+  return _locationData;
+}
